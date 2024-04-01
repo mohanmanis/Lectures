c759f3dac6ec42da3260e38a575b795ce65b0305
Manish Mohan <mohanmanis@gmail.com>
2024-04-01 09:46:08 +0530

    Added medianFromDataStream

---
+++ src/blind75/medianFromDataStream.js
@@ -0,0 +1,131 @@
+class Heap {
+  constructor(compareFn) {
+    this.compareFn = compareFn || ((a, b) => a - b);
+    this.heap = [];
+  }
+
+  getParentIndex = i => Math.floor((i - 1) / 2);
+  getLeftChild = i => 2 * i + 1;
+  getRightChild = i => 2 * i + 2;
+  swap = (i, j) => [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+  //  negative  -> valid and positive -> invalid case
+  swapNeeded = (parentIndex, childIndex) => this.compareFn(this.heap[parentIndex], this.heap[childIndex]) > 0;
+
+
+  get peek() {
+    return this.heap[0];
+  }
+
+  get size() {
+    return this.heap.length;
+  }
+
+
+  bubbleUp = index => {
+    while (true) {
+      const parentIndex = this.getParentIndex(index);
+      if (index > 0 && this.swapNeeded(parentIndex, index)) {
+        this.swap(parentIndex, index);
+        index = parentIndex;
+      } else {
+        return
+      }
+    }
+  }
+  enqueue = val => {
+    this.heap.push(val);
+    this.bubbleUp(this.heap.length - 1);
+  }
+
+  bubbleDown = index => {
+    while (true) {
+      let swapIndex = index;
+      const leftChild = this.getLeftChild(index);
+      const rightChild = this.getRightChild(index);
+      const size = this.size;
+      if (leftChild < size && this.swapNeeded(swapIndex, leftChild)) {
+        swapIndex = leftChild;
+      }
+      if (rightChild < size && this.swapNeeded(swapIndex, rightChild)) {
+        swapIndex = rightChild;
+      }
+
+      if (swapIndex !== index) {
+        this.swap(swapIndex, index);
+        index = swapIndex;
+      } else {
+        return;
+      }
+    }
+  }
+  dequeue = () => {
+    const top = this.peek;
+    this.swap(0, this.size - 1);
+    this.heap.pop();
+    this.bubbleDown(0);
+    return top;
+  }
+  /* 
+    Time: O(n), Since we are using bubble down
+    We will from backwards because we know most of nodes are there in last level ansd in doing so we will be shifting down less nodes down as most of them are at far end already so bubbledown is prefered over bubbleUp.
+    last -> n / 2 nodes
+    last - 1 => n / 4 nodes
+    ...
+    Ist level => 1 node
+    we have logn levels as 2 ^ h == Total nodes(n) so, taking log will
+    make log2 n == h (height)
+    we start from the last level except the leaf nodes because they won't be bubbling down as they don't have any children.
+    
+  T = 0 * n / 2 + 1 * n / 4 + 2 * n / 8 + ... + log(n - 1) * 1 
+  if we write it up further and we use derivative and taylor series then 
+  final value will come out as n
+  so, T = O(n)
+  */
+  buildHeap = arr => {
+    this.heap = arr;
+    for (let i = Math.floor(this.size / 2) - 1; i >= 0; i--) {
+      this.bubbleDown(i);
+    }
+  }
+
+}
+
+
+
+var MedianFinder = function () {
+  this.left = new Heap((a, b) => b - a); // Max heap
+  this.right = new Heap(); // min heap
+};
+// [1, 3, 8], [4]
+/** 
+ * @param {number} num
+ * @return {void}
+ */
+MedianFinder.prototype.addNum = function (num) {
+  if (this.left.size === 0 || num <= this.left.peek) {
+    this.left.enqueue(num);
+  } else {
+    this.right.enqueue(num);
+  }
+  // violation in size
+  if (this.left.size > this.right.size + 1) {
+    this.right.enqueue(this.left.dequeue(num));
+  } else if (this.left.size < this.right.size) {
+    this.left.enqueue(this.right.dequeue(num));
+  }
+  // 3log(n) === logn
+};
+
+/**
+ * @return {number}
+ */
+MedianFinder.prototype.findMedian = function () {
+  return this.left.size > this.right.size ? this.left.peek : (this.left.peek + this.right.peek) / 2;
+};
+
+/** 
+ * Your MedianFinder object will be instantiated and called as such:
+ * var obj = new MedianFinder()
+ * obj.addNum(num)
+ * var param_2 = obj.findMedian()
+ */
