622e756756911f2ddb33e763c3e34fefc77a1d6a
Manish Mohan <mohanmanis@gmail.com>
2024-04-01 21:35:42 +0530

    Added linkedlistcycle

---
+++ src/blind75/linkedListCycle.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  if (!head) return false;
+  let slow = head, fast = head.next;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true
+  }
+  return false
+};
